97d3702c1e996781d456f702fb0361f076e9d9ce
karlsome <ranilekarlkenneth@sasakicoating.com>
2025-11-13 16:54:17 +0900

    production mode

--- labelComparator.js
+++ labelComparator.js
@@ -1,5 +1,5 @@
-//const serverURL = "https://kurachi.onrender.com";
-const serverURL = "http://localhost:3000";
+const serverURL = "https://kurachi.onrender.com";
+//const serverURL = "http://localhost:3000";
 
 // Enable debug mode for troubleshooting
 const DEBUG_MODE = true;
